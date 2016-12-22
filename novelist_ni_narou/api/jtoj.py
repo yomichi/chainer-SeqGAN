173d027a960537bd6ba020aac2dd862460327c09
NOKUBI Takatsugu <knok@daionet.gr.jp>
2016-12-22 15:38:17 +0900

    narou json to the script's json

---
+++ novelist_ni_narou/api/jtoj.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+
+import json
+import glob
+from natto import MeCab
+
+m = MeCab('-Owakati')
+
+o = []
+files = glob.glob("g*.json")
+for fname in files:
+    with open(fname, 'r') as f:
+        j = json.load(f)
+    #
+    for x in j:
+        if not 'story' in x.keys():
+            continue
+        #text = m.parse(x['story']).split()
+        text = x['story']
+        o.append({'summary': text})
+            
+    #
+with open('../dataset/output.json', 'w') as f:
+    json.dump(o, f)
+
